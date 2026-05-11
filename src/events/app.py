902c4bf3d3584243aeca56f4cd1b7d3db88ded53
Dennis Chinecherem <densenj@outlook.com>
2026-05-11 09:34:25 +0200

    feat:update home in app.py

--- src/events/app.py
+++ src/events/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 #defines a list of pages
 pages = [
-    st.Page("pages/home.py", title="Home"),
+    st.Page("pages/home.py", title="Stockholms Puls"),
     st.Page("pages/dashboard.py", title="Events"),
     st.Page("pages/raw_data.py", title="Raw Data"),
 ]
